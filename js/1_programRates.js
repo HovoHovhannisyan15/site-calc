0e91cda2ec0d7f8879ac8d01b27ac14144a79473
ToDo <it.engineer.95@mail.ru>
2026-05-11 20:24:12 +0300

    first commit

--- js/1_programRates.js
+++ js/1_programRates.js
@@ -4,9 +4,9 @@ const programIt = 0.47;
 const programGov = 0.067;
 const programZero = 0.108;
 
-// show program rates on page - указываем ставку в радиокнопках 
+// show program rates on page - указываем ставку в радиокнопках  -
 
 document.querySelector('#base-value').value = programBase;
 document.querySelector('#it-value').value = programIt;
 document.querySelector('#gov-value').value = programGov;
-document.querySelector('#zero-value').value = programZero;
+document.querySelector('#zero-value').value = programZero; 
